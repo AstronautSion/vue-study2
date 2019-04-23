270a4776465efd7cad8389f38eea0df0f3e64fd8
astronaut <astronaut.sion@gmail.com>
2019-04-23 21:59:27 +0900

    create vue cli3 and study router

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NewsView  from '../views/NewsView'
+import AskView from '../views/AskView.vue'
+import JobsView from '../views/JobsView.vue'
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes: [
+    {
+      path: '/news', //path : url 주소
+      component : NewsView, // component : url 주소로 갔을 때 표시될 컴포넌트
+    },{
+      path: '/ask',
+      component : AskView,
+    },{
+      path: '/jobs',
+      component : JobsView,
+    },
+
+  ]
+});
